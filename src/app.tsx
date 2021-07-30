0aa631fac522e7136179f0ad021d327c10a63c75
Alex Tedesco <alextede8899@gmail.com>
2021-07-30 00:20:51 -0400

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+function render() {
+  ReactDOM.render(
+    <div>
+      <p>See this it is working!?!?</p>
+    </div>
+  
+  , document.body
+  );
+}
+
+render();
